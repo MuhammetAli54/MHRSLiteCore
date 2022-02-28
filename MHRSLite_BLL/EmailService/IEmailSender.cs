e7b8214ca76dc0ea12e36dfbf69d4a2a2e4bb43d
Muhammet Ali <94957965+MuhammetAli54@users.noreply.github.com>
2022-02-28 13:09:36 +0300

    Hastanın randevusunu pdf halde göndermek için metotlar eklendi

--- MHRSLite_BLL/EmailService/IEmailSender.cs
+++ MHRSLite_BLL/EmailService/IEmailSender.cs
@@ -1,4 +1,5 @@
 ﻿using MHRSLite_EL;
+using MHRSLite_EL.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,9 @@ namespace MHRSLite_BLL.EmailService
     public interface IEmailSender
     {
         Task SendAsync(EmailMessage message);
+
+        void SendAppointmentPdf(EmailMessage message, AppointmentVM data);
+
+        Task SendAppointmentPdfAsync(EmailMessage message, AppointmentVM data);
     }
 }
