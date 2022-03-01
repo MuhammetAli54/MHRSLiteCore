ae926edd83209de360d2b322cdc108a85dabc9c4
Muhammet Ali <94957965+MuhammetAli54@users.noreply.github.com>
2022-03-01 08:59:16 +0300

    PaginatedLİst Constructorda kaynak liste kendisine addrange ile eklendi.

--- MHRSLite_EL/PagingListModels/PaginatedList.cs
+++ MHRSLite_EL/PagingListModels/PaginatedList.cs
@@ -15,6 +15,7 @@ namespace MHRSLite_EL.PagingListModels
         {
             PageIndex = pageindex;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            this.AddRange(items);
             ItemList = items;
         }
         public bool PreviousPage
